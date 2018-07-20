5f9b22a891741d6f51f6c5ce9a14edccb5a512d3
Luciano Serafini <serafini@fbk.eu>
2018-07-20 13:41:28 +0200

    revised version of the smoking friends cancer example

--- examples_ltn/smokes_friends_cancer.py
+++ examples_ltn/smokes_friends_cancer.py
@@ -1,11 +1,29 @@
 import tensorflow as tf
+import numpy as np
 import logictensornetworks as ltn
-from logictensornetworks import Not,And,Implies,Forall,Exists
+import matplotlib.pyplot as plt
+from logictensornetworks import Not,And,Implies,Forall,Exists,Equiv
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+pd.set_option('display.width', 1000)
+pd.options.display.float_format = '{:,.2f}'.format
+
+def plt_heatmap(df):
+    plt.pcolor(df)
+    plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+    plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
+    plt.colorbar()
+
+
+
+pd.set_option('precision',2)
 
 ltn.LAYERS = 4
-ltn.BIAS_factor = 1e-3
+ltn.BIAS_factor = 1e-7
+ltn.set_universal_aggreg("mean")
 
-size = 10
+size = 20
 g1 = {l:ltn.constant(l,min_value=[0.]*size,max_value=[1.]*size) for l in 'abcdefgh'}
 g2 = {l:ltn.constant(l,min_value=[0.]*size,max_value=[1.]*size) for l in 'ijklmn'}
 g = {**g1,**g2}
@@ -20,6 +38,11 @@ cancer = ['a','e']
 p = ltn.variable("p",tf.concat(list(g.values()),axis=0))
 q = ltn.variable("q",tf.concat(list(g.values()),axis=0))
 
+p1 = ltn.variable("p1",tf.concat(list(g1.values()),axis=0))
+q1 = ltn.variable("q1",tf.concat(list(g1.values()),axis=0))
+
+p2 = ltn.variable("p2",tf.concat(list(g2.values()),axis=0))
+q2 = ltn.variable("q2",tf.concat(list(g2.values()),axis=0))
 
 Friends = ltn.predicate('Friends',size*2)
 Smokes = ltn.predicate('Smokes',size)
@@ -36,28 +59,41 @@ facts = [Friends(g[x],g[y]) for (x,y) in friends]+\
         [Cancer(g[x]) for x in cancer]+\
         [Not(Cancer(g[x])) for x in g1 if x not in cancer] +\
         [Forall(p,Not(Friends(p,p))),
-         Forall((p,q),Implies(Friends(p,q),Friends(q,p))),
-         Forall(p,Exists(q,Friends(p,q))),
-         Forall((p,q),Implies(Friends(p,q),Implies(Smokes(p),Smokes(q)))),
-         Forall(p,Implies(Smokes(p),Cancer(p)))]
+         Forall((p,q),Equiv(Friends(p,q),Friends(q,p))),
+         Equiv(Forall(p1,Implies(Smokes(p1),Cancer(p1))),
+               Forall(p2,Implies(Smokes(p2),Cancer(p2)))),
+         Equiv(Forall(p1,Implies(Cancer(p1),Smokes(p1))),
+               Forall(p2,Implies(Cancer(p2),Smokes(p2))))]
 
-
-loss = tf.reduce_mean(tf.concat(facts,axis=0))
+loss = 1.0/tf.reduce_mean(1/tf.concat(facts,axis=0))
 opt = tf.train.RMSPropOptimizer(learning_rate=.01,decay=.9)
 optimize = opt.minimize(-(loss+ltn.BIAS))
+
 with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for i in range(10000):
                 sess.run(optimize)
                 if i % 100 == 0:
                     print(i,"=====>",sess.run(loss),sess.run(ltn.BIAS))
-        for gg in [g1,g2]:
-            for x in gg:
-                print(x," = ",sess.run(g[x]))
-                print("Cancer("+x+")",sess.run(Cancer(g[x])))
-                print("Smokes("+x+")",sess.run(Smokes(g[x])))
-                for y in gg:
-                    print("Friends("+x+","+y+")",sess.run(Friends(g[x],g[y])))
+        df_smokes_cancer = pd.DataFrame(sess.run(tf.concat([Smokes(p),Cancer(p)],axis=1)),
+                           columns=["Smokes","Cancer"],
+                           index=list('abcdefghijklmn'))
+        pred_friends = sess.run(tf.squeeze(Friends(p,q)))
+        df_friends_ah = pd.DataFrame(pred_friends[:8,:8],
+                           index=list('abcdefgh'),
+                           columns=list('abcdefgh'))
+        df_friends_in = pd.DataFrame(pred_friends[8:,8:],
+                           index=list('ijklmn'),
+                           columns=list('ijklmn'))
+        plt.figure(figsize=(17,5))
+        plt.subplot(131)
+        plt_heatmap(df_smokes_cancer)
+        plt.subplot(132)
+        plt_heatmap(df_friends_ah)
+        plt.subplot(133)
+        plt_heatmap(df_friends_in)
+        plt.show()
+
         print("forall x ~Friends(x,x)",
               sess.run(Forall(p,Not(Friends(p,p)))))
         print("Forall x Smokes(x) -> Cancer(x)",
@@ -66,9 +102,12 @@ with tf.Session() as sess:
               sess.run(Forall((p,q),Implies(Friends(p,q),Friends(q,p)))))
         print("forall x Exists y (Friends(x,y)",
               sess.run(Forall(p,Exists(q,Friends(p,q)))))
-        print("Forall x,y Friends(x,y) -> (Smokes(x)->Smokes(y)",
+        print("Forall x,y Friends(x,y) -> (Smokes(x)->Smokes(y))",
               sess.run(Forall((p,q),Implies(Friends(p,q),Implies(Smokes(p),Smokes(q))))))
-
+        print("Forall x: smokes(x) -> forall y: friend(x,y) -> smokes(y))",
+              sess.run(Forall(p,Implies(Smokes(p),
+                                        Forall(q,Implies(Friends(p,q),
+                                                         Smokes(q)))))))
 
 
 
--- examples_ltnw/smokes_friends_cancer.py
+++ examples_ltnw/smokes_friends_cancer.py
@@ -3,74 +3,138 @@ import logging
 logger = logging.getLogger()
 logger.basicConfig = logging.basicConfig(level=logging.DEBUG)
 
-import tensorflow as tf
-import logictensornetworks as ltn
 import logictensornetworks_wrapper as ltnw
+import tensorflow as tf
+import numpy as np
+import logictensornetworks as ltn
+import matplotlib.pyplot as plt
+from logictensornetworks import Not,And,Implies,Forall,Exists,Equiv
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+pd.set_option('display.width', 1000)
+pd.options.display.float_format = '{:,.2f}'.format
+
+def plt_heatmap(df):
+    plt.pcolor(df)
+    plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+    plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
+    plt.colorbar()
 
 ltn.LAYERS = 4
-ltn.BIAS_factor = 1e-3
-
+ltn.BIAS_factor = -1e-8
+ltn.set_universal_aggreg("mean")
 size = 10
 
-g1='abcdefgh'
-g2='ijklmn'
-g=g1+g2
 for l in 'abcdefghijklmn':
     ltnw.constant(l,min_value=[0.]*size,max_value=[1.]*size)
 
-friends = [('a','b'),('a','e'),('a','f'),('a','g'),('b','c'),('c','d'),('e','f'),('g','h'),
-           ('i','j'),('j','m'),('k','l'),('m','n')]
-smokes = ['a','e','f','g','j','n']
-cancer = ['a','e']
-
 ltnw.variable("p",tf.concat(list(ltnw.CONSTANTS.values()),axis=0))
-ltnw.variable("q",tf.concat(list(ltnw.CONSTANTS.values()),axis=0))
+ltnw.variable("q",ltnw.VARIABLES["p"])
+ltnw.variable("p1",tf.concat([ltnw.CONSTANTS[l] for l in "abcdefgh"],axis=0))
+ltnw.variable("q1",ltnw.VARIABLES["p1"])
+ltnw.variable("p2",tf.concat([ltnw.CONSTANTS[l] for l in "ijklmn"],axis=0))
+ltnw.variable("q2",ltnw.VARIABLES["p2"])
 
 ltnw.predicate('Friends',size*2)
 ltnw.predicate('Smokes',size)
 ltnw.predicate('Cancer',size)
 
-[ltnw.formula("Friends(%s,%s)" %(x,y)) for (x,y) in friends]
-[ltnw.formula("~Friends(%s,%s)" %(x,y)) for x in g1 for y in g1 if (x,y) not in friends and x < y]
-[ltnw.formula("~Friends(%s,%s)" %(x,y)) for x in g2 for y in g2 if (x,y) not in friends and x < y]
-
-[ltnw.formula("Smokes(%s)" % x) for x in smokes]
-[ltnw.formula("~Smokes(%s)" % x) for x in g if x not in smokes]
-
-[ltnw.formula("Cancer(%s)" % x) for x in cancer]
-[ltnw.formula("~Cancer(%s)" % x) for x in g if x not in cancer]
-
-
+ltnw.formula("Friends(a,b)")
+ltnw.formula("~Friends(a,c)")
+ltnw.formula("~Friends(a,d)")
+ltnw.formula("Friends(a,e)")
+ltnw.formula("Friends(a,f)")
+ltnw.formula("Friends(a,g)")
+ltnw.formula("~Friends(a,h)")
+ltnw.formula("Friends(b,c)")
+ltnw.formula("~Friends(b,d)")
+ltnw.formula("~Friends(b,e)")
+ltnw.formula("~Friends(b,f)")
+ltnw.formula("~Friends(b,g)")
+ltnw.formula("~Friends(b,h)")
+ltnw.formula("Friends(c,d)")
+ltnw.formula("~Friends(c,e)")
+ltnw.formula("~Friends(c,f)")
+ltnw.formula("~Friends(c,g)")
+ltnw.formula("~Friends(c,h)")
+ltnw.formula("~Friends(d,e)")
+ltnw.formula("~Friends(d,f)")
+ltnw.formula("~Friends(d,g)")
+ltnw.formula("~Friends(d,h)")
+ltnw.formula("Friends(e,f)")
+ltnw.formula("~Friends(e,g)")
+ltnw.formula("~Friends(e,h)")
+ltnw.formula("~Friends(f,g)")
+ltnw.formula("~Friends(f,h)")
+ltnw.formula("Friends(g,h)")
+ltnw.formula("Friends(i,j)")
+ltnw.formula("~Friends(i,k)")
+ltnw.formula("~Friends(i,l)")
+ltnw.formula("~Friends(i,m)")
+ltnw.formula("~Friends(i,n)")
+ltnw.formula("~Friends(j,k)")
+ltnw.formula("~Friends(j,l)")
+ltnw.formula("Friends(j,m)")
+ltnw.formula("~Friends(j,n)")
+ltnw.formula("Friends(k,l)")
+ltnw.formula("~Friends(k,m)")
+ltnw.formula("~Friends(k,n)")
+ltnw.formula("~Friends(l,m)")
+ltnw.formula("~Friends(l,n)")
+ltnw.formula("Friends(m,n)")
+ltnw.formula("Smokes(a)")
+ltnw.formula("~Smokes(b)")
+ltnw.formula("~Smokes(c)")
+ltnw.formula("~Smokes(d)")
+ltnw.formula("Smokes(e)")
+ltnw.formula("Smokes(f)")
+ltnw.formula("Smokes(g)")
+ltnw.formula("~Smokes(h)")
+ltnw.formula("~Smokes(i)")
+ltnw.formula("Smokes(j)")
+ltnw.formula("~Smokes(k)")
+ltnw.formula("~Smokes(l)")
+ltnw.formula("~Smokes(m)")
+ltnw.formula("Smokes(n)")
+ltnw.formula("Cancer(a)")
+ltnw.formula("~Cancer(b)")
+ltnw.formula("~Cancer(c)")
+ltnw.formula("~Cancer(d)")
+ltnw.formula("Cancer(e)")
+ltnw.formula("~Cancer(f)")
+ltnw.formula("~Cancer(g)")
+ltnw.formula("~Cancer(h)")
 ltnw.formula("forall p: ~Friends(p,p)")
-ltnw.formula("forall p,q:Friends(p,q) -> Friends(q,p)")
-ltnw.formula("forall p: exists q: Friends(p,q)")
-ltnw.formula("forall p,q:Friends(p,q) -> (Smokes(p)->Smokes(q))")
-ltnw.formula("forall p: Smokes(p) -> Cancer(p)")
-    
-ltnw.SESSION=tf.Session() 
-loss = tf.reduce_mean(tf.concat(list(ltnw.FORMULAS.values()),axis=0))
-opt = tf.train.RMSPropOptimizer(learning_rate=.01,decay=.9)
-optimize = opt.minimize(-(loss+ltn.BIAS))
+ltnw.formula("forall p,q:Friends(p,q) % Friends(q,p)")
+ltnw.formula("forall p1:Cancer(p1) % forall p2:Cancer(p2)")
+ltnw.formula("forall p1:(Smokes(p1) -> Cancer(p1)) % forall p2:(Smokes(p2) -> Cancer(p2))")
+ltnw.formula("forall p1:(Cancer(p1) -> Smokes(p1)) % forall p2:(Cancer(p2) -> Smokes(p2))")
 
-ltnw.SESSION.run(tf.global_variables_initializer())
-for i in range(10000):
-        ltnw.SESSION.run(optimize)
-        if i % 100 == 0:
-            sat_level=ltnw.SESSION.run(loss)
-            print(i,"=====>",sat_level,ltnw.SESSION.run(ltn.BIAS))
-            if sat_level >.99:
-                break
+ltnw.initialize_knowledgebase(
+    optimizer=tf.train.RMSPropOptimizer(learning_rate=0.01,decay=.9),
+    formula_aggregator=lambda *x: 1./tf.reduce_mean(1./tf.concat(x,axis=0)))
+ltnw.train(max_epochs=10000)
 
-for x in g:
-    print(x," = ",ltnw.ask(x).squeeze())
-    print("Cancer("+x+"): %.2f" % ltnw.ask("Cancer(%s)" % x).squeeze())
-    print("Smokes("+x+"): %.2f" % ltnw.ask("Smokes(%s)" % x).squeeze())
+df_smokes_cancer = pd.DataFrame(np.concatenate([ltnw.ask("Smokes(p)"),
+                                                ltnw.ask("Cancer(p)")],axis=1),
+                   columns=["Smokes","Cancer"],
+                   index=list('abcdefghijklmn'))
 
-    for y in g:
-        print("Friends("+x+","+y+"): %.2f" % ltnw.ask("Friends(%s,%s)" % (x,y)).squeeze())
-
-for formula in ["forall p: ~Friends(p,p)",
-                "forall p,q: Friends(p,q) -> Friends(q,p)",
-                "forall p: exists q: Friends(p,q)",
-                "forall p,q: Friends(p,q) -> (Smokes(p)->Smokes(q))"]:
-    print(formula,": %.2f" % ltnw.ask(formula).squeeze())
+df_friends_ah = pd.DataFrame(np.squeeze(ltnw.ask("Friends(p1,q1)")),
+                   index=list('abcdefgh'),
+                   columns=list('abcdefgh'))
+df_friends_in = pd.DataFrame(np.squeeze(ltnw.ask("Friends(p2,q2)")),
+                   index=list('ijklmn'),
+                   columns=list('ijklmn'))
+print(df_smokes_cancer)
+print(df_friends_ah)
+print(df_friends_in)
+plt.figure(figsize=(15,4))
+plt.subplot(131)
+plt_heatmap(df_smokes_cancer)
+plt.subplot(132)
+plt_heatmap(df_friends_ah)
+plt.subplot(133)
+plt_heatmap(df_friends_in)
+plt.show()
