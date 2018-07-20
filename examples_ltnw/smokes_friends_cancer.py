29591307fe6bfc3e684e7238a1583245a194690c
Luciano Serafini <serafini@fbk.eu>
2018-07-20 14:03:49 +0200

    added comments to the smoking exanple

--- examples_ltnw/smokes_friends_cancer.py
+++ examples_ltnw/smokes_friends_cancer.py
@@ -2,7 +2,6 @@
 import logging
 logger = logging.getLogger()
 logger.basicConfig = logging.basicConfig(level=logging.DEBUG)
-
 import logictensornetworks_wrapper as ltnw
 import tensorflow as tf
 import numpy as np
@@ -16,6 +15,7 @@ pd.set_option('display.width', 1000)
 pd.options.display.float_format = '{:,.2f}'.format
 
 def plt_heatmap(df):
+    # display the result of a nxm pandas dataframe in a heatmap
     plt.pcolor(df)
     plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
     plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
@@ -23,11 +23,18 @@ def plt_heatmap(df):
 
 ltn.LAYERS = 4
 ltn.BIAS_factor = -1e-8
-ltn.set_universal_aggreg("mean")
-size = 10
+ltn.set_universal_aggreg("mean") # The truth value of forall x p(x) is
+                                 # interpretable as the percentage of
+                                 # element in the range of x that satisties p
 
+embedding_size = 10 # each constant is interperted in a vector of this size
+
+# create on constant for each individual a,b,... i,j, ...
 for l in 'abcdefghijklmn':
-    ltnw.constant(l,min_value=[0.]*size,max_value=[1.]*size)
+    ltnw.constant(l, min_value=[0.] * embedding_size, max_value=[1.] * embedding_size)
+
+
+# create variables that ranges on all the individuals, and the individuals of group 1 and group 2.
 
 ltnw.variable("p",tf.concat(list(ltnw.CONSTANTS.values()),axis=0))
 ltnw.variable("q",ltnw.VARIABLES["p"])
@@ -36,9 +43,14 @@ ltnw.variable("q1",ltnw.VARIABLES["p1"])
 ltnw.variable("p2",tf.concat([ltnw.CONSTANTS[l] for l in "ijklmn"],axis=0))
 ltnw.variable("q2",ltnw.VARIABLES["p2"])
 
-ltnw.predicate('Friends',size*2)
-ltnw.predicate('Smokes',size)
-ltnw.predicate('Cancer',size)
+
+# declare the predicates
+ltnw.predicate('Friends', embedding_size * 2)
+ltnw.predicate('Smokes', embedding_size)
+ltnw.predicate('Cancer', embedding_size)
+
+
+# add the assertional knowledge in our posses
 
 ltnw.formula("Friends(a,b)")
 ltnw.formula("~Friends(a,c)")
@@ -105,16 +117,32 @@ ltnw.formula("Cancer(e)")
 ltnw.formula("~Cancer(f)")
 ltnw.formula("~Cancer(g)")
 ltnw.formula("~Cancer(h)")
+
+# add general constraints on the friendship relation
 ltnw.formula("forall p: ~Friends(p,p)")
 ltnw.formula("forall p,q:Friends(p,q) % Friends(q,p)")
+
+# Add constraints stating that the satisfaction level of the
+# formula forall x smokes(x) -> cancer(x) in group 1 and group 2
+# should be the same. Since in group 1 everything is known, this
+# formula has the effect to transfer the knowledge about group 1
+# to group 2.
+
 ltnw.formula("forall p1:Cancer(p1) % forall p2:Cancer(p2)")
 ltnw.formula("forall p1:(Smokes(p1) -> Cancer(p1)) % forall p2:(Smokes(p2) -> Cancer(p2))")
 ltnw.formula("forall p1:(Cancer(p1) -> Smokes(p1)) % forall p2:(Cancer(p2) -> Smokes(p2))")
 
+# initialize knowledge base
+
 ltnw.initialize_knowledgebase(
     optimizer=tf.train.RMSPropOptimizer(learning_rate=0.01,decay=.9),
     formula_aggregator=lambda *x: 1./tf.reduce_mean(1./tf.concat(x,axis=0)))
+
+# Train the KB
 ltnw.train(max_epochs=10000)
+
+
+# query the KB and display the results
 
 df_smokes_cancer = pd.DataFrame(np.concatenate([ltnw.ask("Smokes(p)"),
                                                 ltnw.ask("Cancer(p)")],axis=1),
