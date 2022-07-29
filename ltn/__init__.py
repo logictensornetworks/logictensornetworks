d15e62f19d4c73efdfb6ea3d2276c6938ce232a2
Samy Badreddine <badreddine.samy@gmail.com>
2022-07-29 15:41:05 +0900

    Added ltn.diag_lock in the init file

--- ltn/__init__.py
+++ ltn/__init__.py
@@ -1,5 +1,5 @@
 from ltn import fuzzy_ops
 from ltn import utils
-from ltn.core import (Predicate, Function, Variable, Constant, Proposition,
-        diag, undiag, as_tensors, Wrapper_Connective, Wrapper_Quantifier, Wrapper_Formula_Aggregator)
+from ltn.core import (Predicate, Function, Variable, Constant, Proposition, diag, undiag,
+        diag_lock, as_tensors, Wrapper_Connective, Wrapper_Quantifier, Wrapper_Formula_Aggregator)
 
